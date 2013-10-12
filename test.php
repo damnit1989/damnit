21d6d0e448812c5dc86eb60a911632937c168152
damnit1989 <damnit1989@163.com>
2013-10-12 21:26:51 +0800

    first add

---
+++ test.php
@@ -0,0 +1,23 @@
+#author lmm
+<?php
+function checkTime($send_time,$limit_days){
+    $begin_time = date("Y-m-d",strtotime($send_time));
+    $end_time = date("Y-m-d");
+    $days = 0;
+    while($begin_time < $end_time){
+        if(!in_array(date("w",strtotime($begin_time)),array('0','1'))){
+            $days++;
+        }
+        echo $days.'<br/>';
+        $begin_time = date("Y-m-d",strtotime($begin_time."+1days"));
+    }
+    if($days <= $limit_days){
+        return true;
+    }
+    return false;
+}
+if(checkTime('2013-10-1',1)){
+    echo 'good';
+}else{
+    echo 'bad';
+}
